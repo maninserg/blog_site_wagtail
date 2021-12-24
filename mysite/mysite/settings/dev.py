ee5825c7d45b231d97e90e65c0dcd5e26faf81a7
maninserg <manin.serg@gmail.com>
2021-12-24 18:12:33 +0300

    Start new project of WagTail

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-k57@*i*@s1tii+6x4m96%t8cohn47q+1md=kmx2%yx0x%%a)y@'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
